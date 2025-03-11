12532b5d3087ffefac3c2c8cc2bb1315cc2a9506
merkiyashev <merkiyashev@nntc.pro>
2025-03-11 20:51:11 +0700

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/mindbox-test-app/',
   optimizeDeps: {
     include: ['material-ui-icons'],
   },
